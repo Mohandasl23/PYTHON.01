29b14979acd82dd51b666512d3bad116882e32e4
Mohandas Leandro de Albuquerque Gomes <157658861+Mohandasl23@users.noreply.github.com>
2024-04-29 22:57:20 -0300

    Python (Pycharm)
    Desafios feitos no pycharm.

---
+++ Pycharm/Desafio 004.py
@@ -0,0 +1,10 @@
+var1  =  input ( 'escreva algo' )
+print ( 'o tipo primitivo desse valor é' , type ( var1 ))
+print ( 'só tem espaços? ' , var1.isspace ( ) )
+print ( 'é um número? ' , var1 . isnumeric ())
+print ( 'é alfabético? ' , var1 . isalpha ())
+print ( 'é alfanumérico? ' , var1.isalnum ( ) )
+print ( 'está em seus superiores? ' , var1 . isupper ())
+print ( 'esta é minúscula? ' , var1 . islower ())
+print ( 'está maiúscula? ' , var1.istitle ( ) )
+
